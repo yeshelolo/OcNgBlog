1824143cf41dbe9a2d6e4f43714952ab854d6472
Laurent SOUEF <laurent.souef@rigpa.org>
2019-10-13 18:53:03 +0200

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'OcNgBlog';
+  title = 'POSTS';
+  posts = [
+    new Post(
+      'Mon Premier Post',
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius possimus quidem voluptas dolor nesciunt neque harum vitae ab dolores repellendus corporis architecto hic officiis voluptatem, ducimus nulla quia perferendis perspiciatis!',
+      2),
+    new Post(
+      'Mon Second Post',
+      'Lorem2 ipsum dolor sit amet, consectetur adipisicing elit. Eius possimus quidem voluptas dolor nesciunt neque harum vitae ab dolores repellendus corporis architecto hic officiis voluptatem, ducimus nulla quia perferendis perspiciatis!',
+      -2),
+    new Post(
+      'Mon Troisième Post',
+      'Lorem3 ipsum dolor sit amet, consectetur adipisicing elit. Eius possimus quidem voluptas dolor nesciunt neque harum vitae ab dolores repellendus corporis architecto hic officiis voluptatem, ducimus nulla quia perferendis perspiciatis!',
+      0),
+  ];
+}
+export class Post {
+    title: string;
+    content: string;
+    loveIts: number;
+    created_at: Date;
+
+    constructor(title: string, content: string, loveIts: number) {
+      this.title = title;
+      this.content = content;
+      this.loveIts = loveIts;
+      this.created_at = new Date();
+    }
 }
---
+++ src/app/post-list-item/post-list-item.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list-item',
+  templateUrl: './post-list-item.component.html',
+  styleUrls: ['./post-list-item.component.css']
+})
+export class PostListItemComponent implements OnInit {
+
+  @Input() post: Post;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getPostClass() {
+    return {
+      'list-group-item-success': this.post.loveIts > 0,
+      'list-group-item-danger': this.post.loveIts < 0,
+    }
+  }
+
+  getTextClass() {
+    return {
+      'text-success': this.post.loveIts > 0,
+      'text-danger': this.post.loveIts < 0,
+    }
+  }
+
+  onBtnLoveClick() {
+    this.post.loveIts++;
+  }
+
+  onBtlDontClick() {
+    this.post.loveIts--;
+  }
+
+}
